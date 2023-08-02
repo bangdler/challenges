230d8a9a5def48bb5dfa1641f9e34b6aba74a8a4
bangdler <zbthz90@gmail.com>
2023-08-02 22:26:15 +0900

    feat: 시계 대략적인 UI 구현

--- 2-digital-clock/project/src/App.tsx
+++ 2-digital-clock/project/src/App.tsx
@@ -1,9 +1,103 @@
+import styled, { css } from 'styled-components';
+import FourDigitClock from '@/components/FourDigitClock';
+import useClock from '@/hooks/useClock';
+import { useState } from 'react';
+import Flex from '@/components/common/Flex';
+
 function App() {
+  const { mode, hourToString, minToString, modeHourToString } = useClock();
+
+  const timeNums = [...hourToString.split(''), ...minToString.split('')].map(
+    Number,
+  );
+
+  const modeTimeNums = [
+    ...modeHourToString.split(''),
+    ...minToString.split(''),
+  ].map(Number);
+
+  const [isMode, setIsMode] = useState(false);
+
   return (
-    <div>
-     app
-    </div>
-  )
+    <Layout>
+      <OuterBox column justifyCenter alignCenter>
+        <InnerBox justifyCenter alignCenter>
+          <FourDigitClock fourNum={isMode ? modeTimeNums : timeNums} />
+          {isMode && <Mode>{mode}</Mode>}
+        </InnerBox>
+        <Wrapper>
+          <div>toggle</div>
+          <ButtonBox>
+            <Button $active={isMode} onClick={() => setIsMode(true)}>
+              12
+            </Button>
+            <Button $active={!isMode} onClick={() => setIsMode(false)}>
+              24
+            </Button>
+          </ButtonBox>
+        </Wrapper>
+      </OuterBox>
+    </Layout>
+  );
 }
 
-export default App
+const Layout = styled.div`
+  height: 100vh;
+  border: 1px solid black;
+`;
+
+const Box = styled(Flex)`
+  border: 1px solid black;
+  border-radius: 14px;
+`;
+
+const OuterBox = styled(Box)`
+  position: relative;
+  margin: 100px auto;
+  width: 650px;
+  height: 420px;
+  gap: 30px;
+`;
+
+const InnerBox = styled(Box)`
+  position: relative;
+  width: 480px;
+  height: 220px;
+`;
+
+const Mode = styled.p`
+  position: absolute;
+  right: 14px;
+  bottom: 30px;
+  font-size: 3rem;
+  color: orangered;
+`;
+
+const Wrapper = styled(Flex)`
+  width: 480px;
+  justify-content: space-between;
+`;
+
+const ButtonBox = styled(Box)`
+  padding: 2px;
+  gap: 2px;
+`;
+
+const Button = styled.button<{ $active: boolean }>`
+  width: 50px;
+  height: 50px;
+  border-radius: 14px;
+  background-color: gainsboro;
+  &:hover {
+    background-color: palegoldenrod;
+  }
+  ${({ $active }) => {
+    if ($active) {
+      return css`
+        background-color: palegoldenrod;
+      `;
+    }
+  }}
+`;
+
+export default App;
