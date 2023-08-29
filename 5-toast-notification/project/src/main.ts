303384d4755a648338e3a2d636b2217c187d9c38
bangdler <zbthz90@gmail.com>
2023-08-29 23:40:03 +0900

    feat: index.html 및 main.ts 에 컴포넌트 생성 로직 추가

--- 5-toast-notification/project/src/main.ts
+++ 5-toast-notification/project/src/main.ts
@@ -1,7 +1,21 @@
 import '@/index.css';
+import { NotificationData } from '@/constants';
+import NotificationButton from '@/notificationButton';
+import Notification from '@/notification';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class='text-3xl font-bold underline'>
-    hello world
-  </div>
-`;
+const $notificationBtnBox = document.querySelector<HTMLDivElement>(
+  '#notificationBtnBox',
+)!;
+const $notificationStack =
+  document.querySelector<HTMLDivElement>('#notificationStack')!;
+
+for (let type in NotificationData) {
+  new NotificationButton($notificationBtnBox, type);
+}
+
+$notificationBtnBox.addEventListener('click', e => {
+  const target = e.target as HTMLElement;
+  if (target.dataset.type) {
+    new Notification($notificationStack, target.dataset.type);
+  }
+});
