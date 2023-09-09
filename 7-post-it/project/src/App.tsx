5df52935b85ab52bc2f5b3061e8dc9153b669619
bangdler <zbthz90@gmail.com>
2023-09-09 16:44:36 +0900

    feat: App 컴포넌트에 드래그앤드롭 초안 반영

--- 7-post-it/project/src/App.tsx
+++ 7-post-it/project/src/App.tsx
@@ -1,5 +1,44 @@
+import { DragDropProvider, Draggable, Droppable } from '@/components/DragDrop';
+import { useState } from 'react';
+
 function App() {
-  return <div>App</div>;
+  const [postItList, setPostItList] = useState([
+    { position: [0, 0] },
+    { position: [30, 30] },
+  ]);
+
+  return (
+    <div>
+      App
+      <DragDropProvider>
+        <Droppable
+          onMouseup={(_, curDraggableId, nextPosition) => {
+            const newPostItList = postItList.map((it, idx) => {
+              if (idx === curDraggableId) {
+                it.position = nextPosition;
+              }
+              return it;
+            });
+            setPostItList(newPostItList);
+          }}
+        >
+          <div className={'w-[400px] h-[400px] border border-black'}>
+            {postItList.map((postIt, idx) => (
+              <Draggable
+                key={idx}
+                draggableId={idx}
+                initPosition={postIt.position as [number, number]}
+              >
+                <div className={'w-[100px] border border-black select-none'}>
+                  draggable {idx}
+                </div>
+              </Draggable>
+            ))}
+          </div>
+        </Droppable>
+      </DragDropProvider>
+    </div>
+  );
 }
 
 export default App;
